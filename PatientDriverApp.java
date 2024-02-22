78a09abc1e57d82274319428f3a5e44357c751f3
sousannaC <157743418+sousannaC@users.noreply.github.com>
2024-02-22 17:28:13 -0500

    Update PatientDriverApp.java

--- PatientDriverApp.java
+++ PatientDriverApp.java
@@ -1,3 +1,18 @@
+/*
+ * Class: CMSC203 
+ * Instructor:Dr.Grinberg 
+ * Assignment 2
+ * Description: 
+*Patient:User inputs name, address, phone number and emergency contact information. 
+*Procedure:Describe the name, date, practitioner, and cost of procedure.
+*PatientDriverApp: Using the information inputed by user, display information in a more user friendly way. 
+ * Due: 02/26/2024
+ * Platform/compiler: Eclipse
+ * I pledge that I have completed the programming 
+ * assignment independently. I have not copied the code from a student or any source. I have not given my code 
+ * to any student.
+   Print your Name here:Sousanna Chugunova
+*/
 package patient;
 import java.util.Random;
 
